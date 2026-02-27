5da6c4597e2b3a7eecc71e9b9bef86d885256839
kim007-lol <ngabdullahhakim99@gmail.com>
2026-02-27 19:15:06 +0700

    ui: dynamic layout and routes for categories edit view

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,4 +1,8 @@
-@extends('layouts.app')
+@php
+$routePrefix = (auth()->check() && auth()->user()->role === 'kasir') ? 'cashier.' : '';
+$layout = $routePrefix ? 'layouts.cashier' : 'layouts.app';
+@endphp
+@extends($layout)
 
 @section('title', 'Edit Kategori')
 
@@ -6,7 +10,7 @@
 <div class="row justify-content-center">
     <div class="col-md-6">
         <h2 class="mb-4">Edit Kategori</h2>
-        <form action="{{ route('categories.update', $category) }}" method="POST">
+        <form action="{{ route($routePrefix . 'categories.update', $category) }}" method="POST">
             @csrf
             @method('PUT')
             <div class="mb-3">
@@ -18,7 +22,7 @@
             </div>
             <div class="d-flex gap-2">
                 <button type="submit" class="btn btn-primary">Simpan</button>
-                <a href="{{ route('categories.index') }}" class="btn btn-secondary">Batal</a>
+                <a href="{{ route($routePrefix . 'categories.index') }}" class="btn btn-secondary">Batal</a>
             </div>
         </form>
     </div>
